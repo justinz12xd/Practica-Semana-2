c7539de65fa2f97ee8cbaad8ec362fd2ba12b1f2
Justindevz <justinalejandro996@gmail.com>
2025-09-26 19:44:22 -0500

    se agrega uuid

--- src/Infraestructure/AnimalRepoMemory.ts
+++ src/Infraestructure/AnimalRepoMemory.ts
@@ -1,3 +1,15 @@
 import { IAnimalRepo } from "../Domain/repositories/ianimal";
 import { Animal } from "../Domain/entities/animal";
+import { v4 as uuidv4 } from "uuid";
 
+
+export class AnimalRepoMemory implements IAnimalRepo {
+    private animals: Animal[] = [];//nuevos animales aqui.
+    
+    //Callback de insert
+    async insert(animal: Animal, callback: (err: Error | null, result?: Animal) => void): Promise<void> {
+        this.animals.push(animal);
+        return callback(null, animal);
+    }
+
+}
